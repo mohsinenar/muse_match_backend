4b20225fa8a7907301409ce4d60010615de0e4d9
mouhcine <mnarmouche@gmal.com>
2023-02-25 20:20:51 +0100

    Add helpers to get spotify

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,6 @@
+import dataclasses
+from typing import Type
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -7,6 +10,22 @@ GENDER_CHOICES = (
 )
 
 
+@dataclasses.dataclass
+class SpotifyAuthData:
+    auth_time: int
+    refresh_token: str
+    access_token: str
+    token_type: str
+
+    @classmethod
+    def from_dict(cls, data_dict):
+        auth_time = data_dict.get("auth_time")
+        refresh_token = data_dict.get("refresh_token")
+        access_token = data_dict.get("access_token")
+        token_type = data_dict.get("token_type")
+        return cls(auth_time=auth_time, refresh_token=refresh_token,access_token=access_token,token_type=token_type)
+
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     looking_for = models.CharField(max_length=6, choices=GENDER_CHOICES)
@@ -14,6 +33,10 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    def spotify(self) -> Type[SpotifyAuthData]:
+        spotify_auth_data = self.user.social_auth.all().get(provider="spotify").extra_data
+        return SpotifyAuthData.from_dict(spotify_auth_data)
 
 
 class MusicGenre(models.Model):
