2510b19dad2dcc56845eb7fdcfe0fa4e6c474683
mouhcine <mnarmouche@gmal.com>
2023-02-28 23:32:21 +0100

    Add postgres database.

--- muse_match/settings.py
+++ muse_match/settings.py
@@ -138,8 +138,12 @@ WSGI_APPLICATION = 'muse_match.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'MUSE_db',
+        'PORT': '5432',
     }
 }
 
